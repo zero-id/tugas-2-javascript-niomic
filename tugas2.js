7c04ed21a4b08464b9f404829ca2f64936edc3e2
zero-id <abdullatiffebriliana15@gmail.com>
2023-03-09 14:54:31 +0700

    tugas 2 js

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var age = 22
+var myName = "Zero"
+var merried = false
+var girlfriend;
+var families = ['mother', 'father', 'younger brother']
+
+console.log(age);
+console.log(myName);
+console.log(merried);
+console.log(girlfriend);
+console.log(families);
